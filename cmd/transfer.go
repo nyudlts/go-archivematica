16a968b7185f01df86f46fc8ba06e98f2d06244c
dmnyu <don.mennerich@nyu.edu>
2023-12-01 11:08:38 -0500

    Updating transfer cmd to batch process directories

--- cmd/transfer.go
+++ cmd/transfer.go
@@ -1,107 +1,142 @@
 package cmd
 
 import (
+	"bufio"
 	"fmt"
 	"go_am"
+	"os"
+	"path/filepath"
+	"strings"
 	"time"
 
 	"github.com/spf13/cobra"
 )
 
-var packageName string
+var (
+	directoryName string
+	writer        *bufio.Writer
+)
 
 const locationName = "amatica rws ingest point"
 
 func init() {
 	transferCmd.Flags().StringVar(&config, "config", "", "")
-	transferCmd.Flags().StringVar(&packageName, "name", "", "")
+	transferCmd.Flags().StringVar(&directoryName, "directory-name", "", "")
 	rootCmd.AddCommand(transferCmd)
 }
 
 var transferCmd = &cobra.Command{
 	Use: "transfer",
 	Run: func(cmd *cobra.Command, args []string) {
+
+		//create a client
 		var err error
 		client, err = go_am.NewAMClient(config, 20)
 		if err != nil {
 			panic(err)
 		}
 
-		location, err := client.GetLocationByName(locationName)
+		//create an output file
+		of, err := os.Create("output.tsv")
+		if err != nil {
+			panic(err)
+		}
+		defer of.Close()
+		writer = bufio.NewWriter(of)
+
+		//process the directory
+		xfrDirs, err := os.ReadDir(directoryName)
 		if err != nil {
 			panic(err)
 		}
 
-		startTransferResponse, err := client.StartTransfer(location.UUID, packageName)
-		if err != nil {
-			panic(err)
-		}
-
-		uuid, err := startTransferResponse.GetUUID()
-		if err != nil {
-			panic(err)
-		}
-
-		fmt.Println("transfer started: " + uuid)
-
-		foundUnapproved := false
-
-		for !foundUnapproved {
-			foundUnapproved = findUnapprovedTransfer(uuid)
-			if !foundUnapproved {
-				fmt.Println("  * Waiting for approval process to complete")
-				time.Sleep(5 * time.Second)
+		for _, xferDir := range xfrDirs {
+			if strings.Contains(xferDir.Name(), "fales") {
+				packageName := filepath.Join("test-dir", xferDir.Name())
+				fmt.Println(packageName)
+				transferPackage(packageName)
 			}
 		}
-
-		//approve the transfer
-
-		transfer, err := client.GetTransferStatus(uuid)
-		if err != nil {
-			panic(err)
-		}
-
-		if err := client.ApproveTransfer(transfer.Directory, "standard"); err != nil {
-			panic(err)
-		}
-
-		approvedTransfer, err := client.GetTransferStatus(uuid)
-		if err != nil {
-			panic(err)
-		}
-
-		fmt.Println("Transfer approved:", approvedTransfer.UUID)
-
-		foundCompleted := false
-		for !foundCompleted {
-			foundCompleted = findCompletedTransfer(uuid)
-			if !foundCompleted {
-				fmt.Println("  * Waiting for transfer process to complete")
-				time.Sleep(5 * time.Second)
-			}
-		}
-
-		completedTransfer, err := client.GetTransferStatus(uuid)
-		if err != nil {
-			panic(err)
-		}
-
-		sipUUID := completedTransfer.SIPUUID
-
-		fmt.Println("Transfer completed: ", sipUUID)
-
-		foundIngestCompleted := false
-		for !foundIngestCompleted {
-			foundIngestCompleted = findCompletedIngest(sipUUID)
-			if !foundIngestCompleted {
-				fmt.Println("  * Waiting for ingest process to complete")
-				time.Sleep(5 * time.Second)
-			}
-		}
-
-		fmt.Println("Ingest Completed:", sipUUID)
 
 	},
+}
+
+func transferPackage(packageName string) {
+	location, err := client.GetLocationByName(locationName)
+	if err != nil {
+		panic(err)
+	}
+
+	startTransferResponse, err := client.StartTransfer(location.UUID, packageName)
+	if err != nil {
+		panic(err)
+	}
+
+	uuid, err := startTransferResponse.GetUUID()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("transfer started: " + uuid + packageName)
+
+	foundUnapproved := false
+
+	for !foundUnapproved {
+		foundUnapproved = findUnapprovedTransfer(uuid)
+		if !foundUnapproved {
+			fmt.Println("  * Waiting for approval process to complete")
+			time.Sleep(5 * time.Second)
+		}
+	}
+
+	//approve the transfer
+
+	transfer, err := client.GetTransferStatus(uuid)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := client.ApproveTransfer(transfer.Directory, "standard"); err != nil {
+		panic(err)
+	}
+
+	approvedTransfer, err := client.GetTransferStatus(uuid)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Transfer approved:", approvedTransfer.UUID)
+
+	foundCompleted := false
+	for !foundCompleted {
+		foundCompleted = findCompletedTransfer(uuid)
+		if !foundCompleted {
+			fmt.Println("  * Waiting for transfer process to complete")
+			time.Sleep(5 * time.Second)
+		}
+	}
+
+	completedTransfer, err := client.GetTransferStatus(uuid)
+	if err != nil {
+		panic(err)
+	}
+
+	sipUUID := completedTransfer.SIPUUID
+
+	fmt.Println("Transfer completed: ", sipUUID)
+
+	foundIngestCompleted := false
+	for !foundIngestCompleted {
+		foundIngestCompleted = findCompletedIngest(sipUUID)
+		if !foundIngestCompleted {
+			fmt.Println("  * Waiting for ingest process to complete")
+			time.Sleep(5 * time.Second)
+		}
+	}
+
+	fmt.Println("Ingest Completed:", sipUUID)
+	writer.WriteString(fmt.Sprintf("%s\t%s\n", sipUUID, packageName))
+	writer.Flush()
 }
 
 func findCompletedIngest(sipUuid string) bool {
