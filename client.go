ebf031b6808485e4049f4cbc9cf4e2f369570eb7
Don Mennerich <don.mennerich@nyu.edu>
2024-02-23 09:20:26 -0500

    adding ss user id and ss user email to am client config

--- client.go
+++ client.go
@@ -10,13 +10,15 @@ import (
 )
 
 type AMClient struct {
-	Username   string
-	AMHost     string
-	AMAPIKey   string
-	SSHost     string
-	SSAPIKey   string
-	Client     *http.Client
-	StagingLoc string
+	Username    string
+	AMHost      string
+	AMAPIKey    string
+	SSHost      string
+	SSAPIKey    string
+	Client      *http.Client
+	StagingLoc  string
+	SSUserID    int
+	SSUserEmail string
 }
 
 func (a *AMClient) String() string {
@@ -24,12 +26,14 @@ func (a *AMClient) String() string {
 }
 
 type AMEnvironment struct {
-	AMURL      string `yaml:"am_url"`
-	SSURL      string `yaml:"ss_url"`
-	Username   string `yaml:"username"`
-	AMAPIKey   string `yaml:"am_api_key"`
-	SSAPIKey   string `yaml:"ss_api_key"`
-	StagingLoc string `yaml:"staging_location"`
+	AMURL       string `yaml:"am_url"`
+	SSURL       string `yaml:"ss_url"`
+	Username    string `yaml:"username"`
+	AMAPIKey    string `yaml:"am_api_key"`
+	SSAPIKey    string `yaml:"ss_api_key"`
+	StagingLoc  string `yaml:"staging_location"`
+	SSUserID    int    `yaml:"ss_user_id"`
+	SSUserEmail string `yaml:"ss_user_email"`
 }
 
 func NewAMClient(config string, timeout int) (*AMClient, error) {
@@ -53,5 +57,14 @@ func NewAMClient(config string, timeout int) (*AMClient, error) {
 		return nil, err
 	}
 
-	return &AMClient{Username: amEnv.Username, AMHost: amEnv.AMURL, SSHost: amEnv.SSURL, AMAPIKey: amEnv.AMAPIKey, SSAPIKey: amEnv.SSAPIKey, Client: nclient, StagingLoc: amEnv.StagingLoc}, nil
+	return &AMClient{
+		Username:    amEnv.Username,
+		AMHost:      amEnv.AMURL,
+		SSHost:      amEnv.SSURL,
+		AMAPIKey:    amEnv.AMAPIKey,
+		SSAPIKey:    amEnv.SSAPIKey,
+		Client:      nclient,
+		StagingLoc:  amEnv.StagingLoc,
+		SSUserID:    amEnv.SSUserID,
+		SSUserEmail: amEnv.SSUserEmail}, nil
 }
